f248cb24ed4cc2b2fef72a58344eb74c86d3962c
Amanda Alexandre <amanda.alexandre@outlook.com>
2023-08-31 15:27:18 -0300

    feat: consulting geocodes and routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react';
 import '../styles/main.css';
 
 function Main() {
+  //User's IP
   const [ip, setIp] = useState("");
+
+  //GeoAPI Info
   const [city, setCity] = useState(null);
   const [country, setCountry] = useState(null)
-  const [currency, setCurrency] = useState(null)
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const geoApiKey = import.meta.env.VITE_GEO_API_KEY;
-  const [destinationCity, setDestinationCity] = useState("");
-  const [mode, setMode] = useState("");
+  const hereApiKey = import.meta.env.VITE_HERE_API_KEY;
 
+  const [selectedCity, setSelectedCity] = useState("");
+  const [searchedCity, setSearchedCity] = useState("");
+  const [matches, setMatches] = useState([]);
+  const [notice, setNotice] = useState("");
+  const [results, setResults] = useState([]);
 
   const options = {
     method: 'GET',
@@ -20,47 +29,78 @@ function Main() {
     }
   }
 
-/*   const handleButtonClick = () => {
-    setLoading(true);
+  // Get user's IP address
+  const getUsersIP = () => {
+    fetch("https://api.ipify.org/?format=json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error getting your IP address!');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setIp(data.ip);
+        setLoading(false);
 
-  } */
+        // Make the second API call using the obtained IP address
+        return fetch(`https://apis.thatapicompany.com/geo-ip-api-community/locations/iplookup?ip=${data.ip}`, options);
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error fetching data from Geo API');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setLat(data.point.lat)
+        setLon(data.point.lon)
+        setCity(data.locationInfo.city);
+        setCountry(data.country.name);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
-    // Get user's IP address
-    const getUsersIP = () => {
-      fetch("https://api.ipify.org/?format=json")
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Error getting your IP address!');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setIp(data.ip);
-          setLoading(false);
-
-          // Make the second API call using the obtained IP address
-          return fetch(`https://apis.thatapicompany.com/geo-ip-api-community/locations/iplookup?ip=${data.ip}`, options);
-        })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Error fetching data from Geo API');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setCity(data.locationInfo.city);
-          setCountry(data.country.name);
-          setCurrency(data.country.currency.symbol)
-        })
-        .catch(error => {
-          setError(error);
-          setLoading(false);
-        });
-    };
-
     getUsersIP();
   }, []);
+
+  const handleCityTyping = async (e) => {
+    const value = e.target.value;
+    setSearchedCity(value)
+    // Perform the fetch for the API call
+    const response = await fetch(`https://geocode.search.hereapi.com/v1/geocode?q=${value}&apiKey=${hereApiKey}`);
+
+    // Check if the response is successful
+    if (response.ok) {
+      const data = await response.json();
+      // Extract the items from the data and setMatches
+      setMatches(data.items);
+    } else {
+      // Handle the case where the fetch fails
+      setMatches([]); // Clear the suggestions
+    }
+  }
+
+  const handleMatchSelection = (match) => {
+    setSelectedCity(match);
+    setMatches([]);
+
+    // Getting destination's geocodes and doing the routing API call
+    fetch(`https://geocode.search.hereapi.com/v1/geocode?q=${match.address.label}&apiKey=${hereApiKey}`)
+    .then(response => response.json())
+    .then(data => fetch(`https://intermodal.router.hereapi.com/v8/routes?apiKey=${hereApiKey}&destination=${data.items[0].position.lat},${data.items[0].position.lng}&origin=${lat},${lon}`))
+    .then(res2 => res2.json())
+    .then(data2 => {
+      console.log(data2)
+      setNotice(data2.notices[0].title)
+      setResults(data2.routes)
+    })
+    .catch(error => console.error('Error fetching geocode data from your chosen city.', error))
+
+  }
 
   if (loading) {
     return <p>Loading...</p>;
@@ -73,29 +113,35 @@ function Main() {
   return (
     <main className='main'>
       <h1>Find your next adventure</h1>
-      <p>Type your transportation + hotel budget estimate and we'll give you a list of possible traveling options. </p>
+      <p>Type your destination and we'll show you how long you'll take to get there. </p>
       <br />
       <span>
-        I wanna go to {currency} &nbsp;
-        <input type='text' placeholder='Type a city' value={destinationCity} onChange={e => setBudget(e.target.value)}></input>
+        I wanna go to &nbsp;
+        <input type='text' placeholder='Enter a city name' value={searchedCity} onChange={handleCityTyping}></input>
       </span>
-      <br />
-
-      <span>by &nbsp;
-        <select name="mode_transportation" id="mode" value={mode} onChange={e=> setMode(e.target.value)}>
-          <option value="car">Car</option>
-          <option value="pedestrian">Foot</option>
-          <option value="scooter">Scooter</option>
-          <option value="taxi">Taxi</option>
-          <option value="bus">Bus</option>
-          <option value="privateBus">Private Bus</option>
-        </select>
-      </span>
-
-      <br />
-      <button>Show me how!</button>
+      
+      {/* Display search suggestions */}
+      {matches.length > 0 && (
+        <div className="search-suggestions">
+          {matches.map((match) => (
+            <div key={match.id} onClick={() => handleMatchSelection(match)}>
+              {match.title} {/* Display the match title or any other relevant data */}
+            </div>
+          ))}
+        </div>
+      )}
+      {/* ... End of search suggestions */}
       <br/>
       (You're currently in {city}, {country}) 
+
+      {notice && (
+        <div className='main-notice'><p>{notice}</p></div>
+      )}
+
+      {results.length > 0 && (
+        results.map(result => <p>{result}</p>)
+      )} 
+
     </main>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import '../styles/navbar.css'
 function Navbar() {
   return (
     <div className='navbar'>
-        <h2>Trip Planner</h2>
+        <h2>Road Trip Planner</h2>
     {/*     Credits
         About me */}
     </div>
