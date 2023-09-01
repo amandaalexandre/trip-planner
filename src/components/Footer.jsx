b21104f52a87c850d783ffadd5140ea442fdf140
Amanda Alexandre <amanda.alexandre@outlook.com>
2023-08-31 21:07:29 -0300

    fix: linkedin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/footer.css'
 function Footer() {
   return (
     <footer>
-        <span>Created by Amanda Alexandre: <a href="linkedin.com/in/amandavieiradev/">LinkedIn</a> <a href="https://github.com/amandaalexandre">Github</a></span>
+        <span>Created by Amanda Alexandre: <a href="https://www.linkedin.com/in/amandavieiradev/">LinkedIn</a> <a href="https://github.com/amandaalexandre">Github</a></span>
         <span>This project was created as an entry for ThatAPICompany's monthly API competition. <a href="https://discord.gg/Hs9HfQGshR">Join their Discord to know more.</a></span>
         
     </footer>
